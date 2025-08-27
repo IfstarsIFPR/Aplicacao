25ad6cb683895be035016c6c3d8931f46dce7402
HeloiseFigueiredo <heloise.ramos.figueiredo.tds.2022@gmail.com>
2025-08-26 22:25:46 -0300

    css só das home
    o que fiz foi apenas colocar as cores que estavam faltando e algumas coisas para não ficar muito vazio

--- app/view/home/homeAluno.php
+++ app/view/home/homeAluno.php
@@ -5,7 +5,7 @@
 require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
 ?>
-<link rel="stylesheet" href="<?= BASEURL ?>/view/css/home.css">
+<link rel="stylesheet" href="<?= BASEURL ?>/view/css/homeAluno.css">
 
 <h3 class="text-center">Página inicial do sistema</h3>
 
