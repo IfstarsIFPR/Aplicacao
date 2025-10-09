9392204f19c915211ba18692e8b61133015c200a
HeloiseFigueiredo <heloise.ramos.figueiredo.tds.2022@gmail.com>
2025-10-09 11:48:13 -0300

    testes css dos usuarios,
    tem algumas mudança no menu tbm

--- app/view/include/menu.php
+++ app/view/include/menu.php
@@ -22,97 +22,101 @@ if (isset($_SESSION[SESSAO_USUARIO_TIPO])) {
 <link rel="stylesheet" href="<?= BASEURL ?>/view/css/menu.css">
 
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom shadow-sm">
-  <div class="container-fluid">
+    <div class="container-fluid">
 
-    <!-- Logo -->
-    <a class="navbar-brand d-flex align-items-center" href="<?= HOME_PAGE ?>">
-      <img src="<?= BASEURL ?>/view/img/logoStars.php" alt="Logo" class="navbar-logo me-2">
-      <span>Home</span>
-    </a>
 
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+        <a class="navbar-brand d-flex align-items-center" href="<?= HOME_PAGE ?>">
+            <img src="<?= BASEURL ?>/view/img/logoStars.png" alt="Logo" class="navbar-logo me-2">
+        </a>
 
-    <!-- Conteúdo do menu -->
-    <div class="collapse navbar-collapse" id="navSite">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <?php if ($isAdmin): ?>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdownCadastros" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Cadastros
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownCadastros">
-              <li><a class="dropdown-item"
-                    href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>">Usuários</a></li>
-              <li><a class="dropdown-item"
-                     href="<?= BASEURL . '/controller/UsuarioController.php?action=create' ?>">Outro cadastro</a></li>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="navSite">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <?php if ($isAdmin): ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownCadastros" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastros
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownCadastros">
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>">Usuários</a></li>
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/UsuarioController.php?action=create' ?>">Outro cadastro</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownCursos" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Gerenciar Cursos
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownCursos">
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/CursoController.php?action=list' ?>">Cursos</a></li>
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/CursoController.php?action=create' ?>">Adicionar curso</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownDisciplinas" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Gerenciar Disciplinas
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownDisciplinas">
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/DisciplinaController.php?action=list' ?>">Disciplinas</a></li>
+                            <li><a class="dropdown-item"
+                                    href="<?= BASEURL . '/controller/DisciplinaController.php?action=create' ?>">Adicionar disciplina</a></li>
+                        </ul>
+                    </li>
+                <?php endif; ?>
+
+                <?php if ($isAluno): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Minhas Turmas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Minhas Avaliações</a>
+                    </li>
+                <?php endif; ?>
+
+                <?php if ($isProfessor): ?>
+                    <li class="nav-item"><a class="nav-link" href="#">Minhas Disciplinas</a></li>
+                <?php endif; ?>
+
             </ul>
-          </li>
+            <ul class="navbar-nav ms-auto mr-3">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarUsuario"
+                        data-bs-toggle="dropdown">
+                    </a>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdownCursos" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Gerenciar Cursos
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownCursos">
-              <li><a class="dropdown-item" 
-                     href="<?= BASEURL . '/controller/CursoController.php?action=list' ?>">Cursos</a></li>
-              <li><a class="dropdown-item" 
-                     href="<?= BASEURL . '/controller/CursoController.php?action=create' ?>">Adicionar curso</a></li>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarUsuario"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="user-avatar"><?= strtoupper(substr($nome, 0, 2)) ?></div>
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUsuario">
+                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/PerfilController.php?action=view' ?>">Perfil</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item text-danger" href="<?= LOGOUT_PAGE ?>">Sair</a></li>
+                    </ul>
+                </li>
             </ul>
-          </li>
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdownDisciplinas" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Gerenciar Disciplinas
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdownDisciplinas">
-              <li><a class="dropdown-item" 
-                     href="<?= BASEURL . '/controller/DisciplinaController.php?action=list' ?>">Disciplinas</a></li>
-              <li><a class="dropdown-item" 
-                     href="<?= BASEURL . '/controller/DisciplinaController.php?action=create' ?>">Adicionar disciplina</a></li>
-            </ul>
-          </li>
-        <?php endif; ?>
-
-        <?php if ($isAluno): ?>
-          <li class="nav-item">
-             <a class="nav-link" href="#">Minhas Turmas</a></li>
-          <li class="nav-item">
-             <a class="nav-link" href="#">Minhas Avaliações</a></li>
-        <?php endif; ?>
-
-        <?php if ($isProfessor): ?>
-          <li class="nav-item"><a class="nav-link" href="#">Minhas Disciplinas</a></li>
-        <?php endif; ?>
-     
-    </ul>
- <ul class="navbar-nav ms-auto mr-3">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarUsuario"
-                    data-bs-toggle="dropdown">
-                    <?= $nome ?>
-                </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarUsuario"
-            role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <div class="user-avatar"><?= strtoupper(substr($nome, 0, 2)) ?></div>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUsuario">
-            <li><a class="dropdown-item" href="<?= BASEURL . '/controller/PerfilController.php?action=view' ?>">Perfil</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item text-danger" href="<?= LOGOUT_PAGE ?>">Sair</a></li>
-          </ul>
-        </li>
-      </ul>
-
+        </div>
     </div>
-  </div>
 </nav>
