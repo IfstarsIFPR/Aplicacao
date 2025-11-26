93c2a5f27d12faa3221fe16c63757f11a2c81b0c
HeloiseFigueiredo <heloise.ramos.figueiredo.tds.2022@gmail.com>
2025-11-25 23:48:23 -0300

    CSS do Perfil dos Usuários

--- app/view/perfil/perfil.php
+++ app/view/perfil/perfil.php
@@ -8,59 +8,59 @@ require_once(__DIR__ . "/../include/menu.php");
 <link rel="stylesheet" href="<?= BASEURL ?>/view/css/perfil.css">
 
 
+<div class="container mt-5">
 
-<div class="container">
+    <h3 class="text-center mb-4">Perfil</h3>
 
-    <h3 class="text-center mt-2">Perfil</h3>
+    <div class="card shadow-lg perfil-card mx-auto p-3">
 
-    <div class="row mt-2">
-        <div class="col-12 mb-2">
-            <span class="fw-bold">Nome:</span>
-            <span><?= $dados['usuario']->getNome() ?></span>
+        <!-- FOTO DO PERFIL -->
+        <div class="text-center mb-3">
+            <?php
+            $foto = $dados['usuario']->getFotoPerfil();
+            $fotoPerfil = $foto
+                ? BASEURL_ARQUIVOS . '/' . $foto
+                : BASEURL . '/view/img/default_profile.png';
+            ?>
+            <img src="<?= $fotoPerfil ?>" class="foto-perfil">
+<br>
+            <h4><strong><?= $dados['usuario']->getNome() ?></h4>
+
         </div>
 
-        <div class="col-12 mb-2">
-            <span class="fw-bold">Email:</span>
-            <span><?= $dados['usuario']->getEmail() ?></span>
-        </div>
+        <!-- INFORMAÇÕES DO USUÁRIO -->
+        <div class="info-mini-card shadow-sm mt-3 p-1 my-3">
 
-        <div class="col-12 mb-2">
-            <span class="fw-bold">Tipo:</span>
-            <span><?= $dados['usuario']->getTipousuario() ?></span>
-        </div>
-
-        <div class="col-12 mb-2">
-            <div class="fw-bold">Foto:</div>
-            <?php if($dados['usuario']->getFotoPerfil()): ?>
-                <img src="<?= BASEURL_ARQUIVOS . '/' . $dados['usuario']->getFotoPerfil() ?>"
-                    height="300">
-            <?php endif; ?>
-        </div>
-
-    </div>
-    
-    <div class="row mb-3 mt-3">
-        
-        <div class="col-12">
-            <form id="frmUsuario" method="POST" 
-                action="<?= BASEURL ?>/controller/PerfilController.php?action=save"
-                enctype="multipart/form-data" >
-                    <label class="form-label" for="txtFoto">Foto de perfil: </label>
-                    <input class="form-control" type="file" 
-                        id="txtFoto" name="foto" />
+            <div class="row">
+                <div class="col-6 text-center">
+                    <p class="titulo-pequeno">Email</p>
+                    <p class="valor-pequeno"><?= $dados['usuario']->getEmail() ?></p>
                 </div>
 
-                <input type="hidden" name="fotoAnterior" value="<?= $dados['usuario']->getFotoPerfil() ?>">
-                
-                <div class="mt-3">
-                    <button type="submit" class="btn btn-success">Gravar</button>
+                <div class="col-6 text-center">
+                    <p class="titulo-pequeno">Tipo</p>
+                    <p class="valor-pequeno"><?= $dados['usuario']->getTipousuario() ?></p>
                 </div>
-            </form>            
+            </div>
+        </div>
+
+        <!-- UPLOAD FOTO -->
+        <form id="frmUsuario" method="POST"
+            action="<?= BASEURL ?>/controller/PerfilController.php?action=save"
+            enctype="multipart/form-data">
+
+            <label class="form-label fw-bold" for="txtFoto">Alterar foto de perfil:</label>
+            <input class="form-control mb-3" type="file" id="txtFoto" name="foto">
+            <input type="hidden" name="fotoAnterior" value="<?= $dados['usuario']->getFotoPerfil() ?>">
+
+            <button type="submit" class="btn btn-primary w-100 mt-2">Salvar</button>
+        </form>
 
     </div>
-</div>
+
 </div>
 
-<?php  
+
+<?php
 require_once(__DIR__ . "/../include/footer.php");
 ?>
